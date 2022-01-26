deaf63a59454f527a5e7f0432c01ae9e35585876
Mateo Ledesma <mateo.14.ledesma@gmail.com>
2022-01-26 14:47:01 -0300

    init

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,10 @@
 import express from 'express';
 import cors from 'cors';
-import postsRouter from './features/posts/posts.router';
 
 const PORT: number = parseInt(process.env.PORT as string) || 3000;
 const app = express();
 
 app.use(cors());
-
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
