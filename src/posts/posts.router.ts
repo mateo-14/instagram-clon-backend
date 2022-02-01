53751a64f31a5155528dd68f75c6eb988221fe27
Mateo Ledesma <mateo.14.ledesma@gmail.com>
2022-02-01 16:51:31 -0300

    Add create post validator

--- src/posts/posts.router.ts
+++ src/posts/posts.router.ts
@@ -9,6 +9,7 @@ import {
 } from './posts.controller';
 import verifyJwtMiddleware from 'common/jwt/jwt.middleware';
 import upload from 'common/multer';
+import createValidator from './validators/create.validator';
 
 const router = Router();
 
@@ -16,7 +17,13 @@ const POST_MAX_IMAGES: number = parseInt(process.env.POST_MAX_IMAGES!) || 1;
 
 router.get('/feed', verifyJwtMiddleware, getFeedPosts);
 router.get('/:id', verifyJwtMiddleware, getPost);
-router.post('/', verifyJwtMiddleware, upload.array('images', POST_MAX_IMAGES), createPost);
+router.post(
+  '/',
+  verifyJwtMiddleware,
+  upload.array('images', POST_MAX_IMAGES),
+  createValidator,
+  createPost
+);
 router.delete('/:id', verifyJwtMiddleware, deletePost);
 router.put('/:id/likes/me', verifyJwtMiddleware, addClientLike);
 router.delete('/:id/likes/me', verifyJwtMiddleware, removeClientLike);
