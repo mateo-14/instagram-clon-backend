3f4ce2fc0942beca822470e5f658d8893f53453c
Mateo Ledesma <mateo.14.ledesma@gmail.com>
2022-02-03 15:52:24 -0300

    Add error property to validation errors

--- src/common/middlewares/validator.middleware.ts
+++ src/common/middlewares/validator.middleware.ts
@@ -3,7 +3,7 @@ import { validationResult } from 'express-validator';
 
 export default function validatorMiddleware(req: Request, res: Response, next: NextFunction) {
   const errors = validationResult(req);
-  if (!errors.isEmpty()) return res.status(400).json({ errors: errors.mapped() });
+  if (!errors.isEmpty()) return res.status(400).json({ errors: errors.mapped(), error: true });
 
   next();
 }
