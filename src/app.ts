05998494f86acea3d6bd772ca7d5e5459e5db75b
Mateo Ledesma <mateo.14.ledesma@gmail.com>
2022-04-30 02:09:20 -0300

    add open api, and add /api path

--- src/app.ts
+++ src/app.ts
@@ -4,17 +4,18 @@ import authRouter from 'auth/auth.router';
 import postsRouter from 'posts/posts.router';
 import usersRouter from 'users/users.router';
 import commentsRouter from 'comments/comments.router';
+import swaggerUi from 'swagger-ui-express';
 
 const PORT: number = parseInt(process.env.PORT as string) || 8080;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use('/auth', authRouter);
-app.use('/posts', postsRouter);
-app.use('/users', usersRouter);
-app.use('/comments', commentsRouter);
-
+app.use('/api/auth', authRouter);
+app.use('/api/posts', postsRouter);
+app.use('/api/users', usersRouter);
+app.use('/api/comments', commentsRouter);
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(require('../openapi.json')));
 app.use((err: Error, _: Request, res: Response, next: NextFunction) => {
   if (res.headersSent) return next(err);
 
